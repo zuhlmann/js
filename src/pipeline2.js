922eede18494cffa1fb1b67380f380e27cca8770
zuhlmann <uhlmannz@gmail.com>
2026-06-17 11:50:30 -0600

    periodic update

--- src/pipeline2.js
+++ src/pipeline2.js
@@ -1,41 +1,6 @@
 //NOTE: IF RECEIVING ERROR: Unable to Open Stream..., move from external HD to C drive (or somewhere)
 //PDAL / JS has issue parsing E:/...  paths ZU 20251215
 
-//Rasterizing m3c2 with threshold
-[
-  "E:/tolt/add_2026/2024_2025/*.las",
-//  {
-//    "type":"filters.range",
-//    "limits":"M3C2 distance![-0.208:0.208]"
-//  },
-//  {
-//    "type":"filters.ferry",
-//    "dimensions":"significant change =>significant_change"
-//  },
-//  {
-//    "type":"filters.expression",
-//    "expression":"significant_change==1"
-//  },
-  {
-    "type":"filters.ferry",
-    "dimensions":"m3c2_distance =>M3C2_distance"
-  },
-  {
-    "type":"filters.assign",
-    "value":"M3C2_distance = 0 WHERE (M3C2_distance > -0.208 && M3C2_distance < 0.208)"
-  },
-  {
-    "type":"writers.gdal",
-    "filename":"2025_2024_M3C2_30norm_6proj_mean_SIG_thresh208.tif",
-    "output_type":"mean",
-    "gdaldriver":"GTiff",
-    "resolution":1.5,
-    "override_srs":"EPSG:6597",
-    "dimension":"M3C2_distance",
-    "binmode":"true",
-    "bounds":"[1394900, 251260, 1430629, 258249]"
-  }
-  ]
 
 ////Ferry CloudCompare Dimensions to PDAL for M3C2
 //[
@@ -68,19 +33,30 @@
 //    }
 //]
 
-////Filter point cloud
-//[
-//  "E:/tolt/2025_Final/2025_LAS/SFT2025_217.las",
-//  {
-//    "type":"filters.expression",
-//    "expression":"Classification==2 || Classification==9 || Classification==2 || Classification==40 || Classification==41 || Classification==45"
-//  },
-//  {
-//    "type":"writers.las",
-//    "filename":"SFT2025_217_FILTERED.las"
-//  }
-//  ]
-//
+//Filter point cloud
+[
+  "E:/tolt/2025_Final/2025_LAS/*.las",
+  {
+    "type":"filters.expression",
+    "expression":"Classification==41 || Classification==45"
+//    "expression":"Classification==40"
+  },
+ {
+    "type":"writers.gdal",
+    "filename":"E:/tolt/2025_Final/2025_Processed_Rasters_McM/SFT2025_Cls41_45_GreenReturns_Mean.tif",
+    "output_type":"mean",
+    "gdaldriver":"GTiff",
+    "resolution":1.5,
+    "override_srs":"EPSG:6597",
+//    binmode (read docs) will respect the resolution - only points within pixel will be considered
+    "binmode":"true",
+//    2025 lidar bounds(mst and sft)
+    "bounds":"[1370458, 233905, 1430560, 257677]"
+//    Just Lower SFT Bounds (all other NV5 years)
+//    "bounds":"[1394900, 251260, 1430629, 258249]"
+  }
+]
+
 ////m3C2 diff
 //  [
 //    {
@@ -120,42 +96,43 @@
 
 ////Generate water surfaces BE
 //[
-//    "E:/tolt/2024_LiDAR/2024_LAS/2024_LAS/Boulder/*.las",
+//    "E:/tolt/add_2026/2020_2014/*.las",
 ////  {
 ////    "type":"filters.ferry",
 ////    "dimensions":"significant change =>significant_change"
 ////  },
 //  {
 //    "type":"filters.expression",
-//    "expression":"Classification==72",
-////    "expression":"significant_change==1",
-//    "tag":"multiple"
+////    "expression":"Classification==72",
+//    "expression":"m3c2_count1>0 && m3c2_count2>0"
+////    "tag":"multiple"
 //   },
-////  {
-////    "type":"filters.assign",
-////    "value":"m3c2_distance = 0 WHERE (m3c2_significant==0)"
-////  },
+//  {
+//    "type":"filters.assign",
+//    "value":"m3c2_distance = 0 WHERE (m3c2_significant==0)"
+//  },
 ////    {
 ////    "type": "writers.las",
-////    "filename": "C:/Box/MCM USERS/3.0 - Employees/zuhlmann/Tutorials/lidar_lightning/2024_0292_Class2_9_73.las",
+//////    "filename": "C:/Box/MCM USERS/3.0 - Employees/zuhlmann/Tutorials/lidar_lightning/2024_0292_Class2_9_73.las",
+////    "filename": "E:/tolt/add_2026/2020_2014/2020_2014_M3C2_30norm_6rad_2020Ref_Mean.las",
 ////    "extra_dims": "all",
 ////    "minor_version": "4"
-////    }
+////    },
 //  {
 //    "type":"writers.gdal",
-//    "filename":"E:/tolt/2024_LiDAR/2024_McM_Processed/SFT2024_Boulder_Class72.tif",
+//    "filename":"E:/tolt/add_2026/2020_2014/2020_2014_M3C2_30norm_6rad_2020Ref_Mean.tif",
 //    "output_type":"mean",
 //    "gdaldriver":"GTiff",
 //    "resolution":1.5,
 ////    "override_srs":"EPSG:6557",
 //    "override_srs":"EPSG:6597",
-////    "dimension":"m3c2_uncertainty",
-//    "binmode":"true",
+//    "dimension":"m3c2_distance",
+//    "binmode":"true"
 ////    2025 lidar bounds(mst and sft)
 ////    "bounds":"[2154740, 1322950, 2156100, 1324220]"
 ////    "bounds":"[2154355, 1320725, 2157455, 1324460]"
 //////    other year bounds
-//    "bounds":"[1394900, 251260, 1430629, 258249]"
+////    "bounds":"[1394900, 251260, 1430629, 258249]"
 //  }
 //]
 
@@ -199,39 +176,39 @@
 //    }
 //    ]
 
-[
-    {
-        "filename":"E:/tolt/add_2026/SUBSTRATE/2024_las_merged_reach1.las",
-        "tag":"A"
-    },
-    {
-        "type": "filters.range",
-        "limits": "Classification[2:2], Classification[72:72]"
-    },
-    {
-        "type": "filters.hag_delaunay"
-    },
-    {
-
-        "type": "filters.ferry",
-        "dimensions": "HeightAboveGround=Z"
-    },
-    {
-        "type": "filters.expression",
-        "expression": "Classification==72"
-    },
-    {
-        "type": "writers.gdal",
-        "filename":"E:/tolt/2024_LiDAR/2024_McM_Processed/SFT2024_Boulder_Class72_HAG.tif",
-        "output_type": "max",
-        "gdaldriver": "GTiff",
-        "nodata": -9999,
-        "resolution": 1.5,
-//        "bounds":"[1394900, 251260, 1430629, 258249]"
-//        Just Reach 1
-        "bounds":"[1396750, 251600, 1401440, 256740]"
-    }
-    ]
+//[
+//    {
+//        "filename":"E:/tolt/add_2026/SUBSTRATE/2024_las_merged_reach1.las",
+//        "tag":"A"
+//    },
+//    {
+//        "type": "filters.range",
+//        "limits": "Classification[2:2], Classification[72:72]"
+//    },
+//    {
+//        "type": "filters.hag_delaunay"
+//    },
+//    {
+//
+//        "type": "filters.ferry",
+//        "dimensions": "HeightAboveGround=Z"
+//    },
+//    {
+//        "type": "filters.expression",
+//        "expression": "Classification==72"
+//    },
+//    {
+//        "type": "writers.gdal",
+//        "filename":"E:/tolt/2024_LiDAR/2024_McM_Processed/SFT2024_Boulder_Class72_HAG.tif",
+//        "output_type": "max",
+//        "gdaldriver": "GTiff",
+//        "nodata": -9999,
+//        "resolution": 1.5,
+////        "bounds":"[1394900, 251260, 1430629, 258249]"
+////        Just Reach 1
+//        "bounds":"[1396750, 251600, 1401440, 256740]"
+//    }
+//    ]
 
 ////Index has location field.  Refer to pipeline2.js for more details
 //[
